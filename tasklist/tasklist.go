f32baabc83acdd26775503ad14963320a3fa255b
Kenneth Liu <ken.liu@gmail.com>
2023-03-24 19:52:03 -0400

    remove quit command from tasklist component

--- tasklist/tasklist.go
+++ tasklist/tasklist.go
@@ -30,12 +30,10 @@ func New() Model {
 	return initialModel(loadTasks())
 }
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
