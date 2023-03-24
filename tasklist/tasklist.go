0cc549b239e772ccfba0fe72895bdd7436d3e5a7
Kenneth Liu <ken.liu@gmail.com>
2023-03-24 17:36:19 -0400

    add quit keybinding to tasklist

--- tasklist/tasklist.go
+++ tasklist/tasklist.go
@@ -34,6 +34,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
